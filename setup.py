8a5bbcda8ed0f4c1973c17245d531488cac8d318
epsalt <evan.saltman@gmail.com>
2020-06-15 21:10:27 -0600

    added requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
 from setuptools import setup, find_packages
 
+requires = [
+    "beautifulsoup4",
+    "cached-property",
+    "Jinja2",
+    "markdown-full-yaml-metadata",
+    "Markdown",
+    "PyYAML",
+    "pygments",
+    "python-dateutil",
+]
+
+
 setup(
     name="litesite",
     version="1.0",
     packages=find_packages(),
     entry_points={"console_scripts": "litesite = litesite.cli:main"},
+    install_requires=requires,
 )
